a247feff82a6f9aaf3468771d042c2964ca325f3
ravinder k <ravinder.naik@photoninfotech.net>
2018-09-05 16:40:56 +0530

    Component part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar , NavbarBrand,Button } from 'reactstrap';
+import { Navbar , NavbarBrand } from 'reactstrap';
+import Menu from "./components/MenuComponent";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
        <Navbar dark color="primary">
        <div className="container">
-       <NavbarBrand href= "/">Demo of Con Fusion </NavbarBrand>
-       <div> 
-       <Button className="refreshButton">Refresh</Button>
-       <Button className="saveButton">Cancel</Button>
-       <Button className="cancelButton">Save</Button>
-       </div>
-       
+       <NavbarBrand href= "/">Demo of Con Fusion </NavbarBrand> 
        </div>
        </Navbar>
+       <Menu />
       </div>
     );
   }
